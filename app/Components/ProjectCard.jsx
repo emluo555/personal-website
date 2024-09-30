8cc52005f13cfeebffb0426631d6a9f5f6e6efe1
emluo555 <emilyluo555@gmail.com>
2024-09-30 03:32:29 -0400

    adding images and formatting for card container

--- app/Components/ProjectCard.jsx
+++ app/Components/ProjectCard.jsx
@@ -1,13 +1,14 @@
 "use client"
 import React, {useState} from 'react';
 import styles from './Projects.module.css';
+import Image from "next/image";
 
-const ProjectCard =(title, imgLink, role, description, tools, githubLink, devpostLink)=>{
-
+const ProjectCard =({title, imgLink, role, description, tools, githubLink, devpostLink})=>{
+    console.log((imgLink))
     return(
         <div className="container">
-            <img></img>
-            <h1>{title}</h1>
+            <img src={imgLink} width={"90%"}  ></img>
+            <h1 style={{fontSize:'100%'}}>{title}</h1>
             <p>{description}</p>
             {tools != "" && 
             <p>
--- app/Components/Projects.jsx
+++ app/Components/Projects.jsx
@@ -1,18 +1,24 @@
 "use client"
 import React, {useState} from 'react';
 import styles from './Projects.module.css';
+import data from '../data/projects.json';
+import ProjectCard from './ProjectCard';
 
 
 const Projects =()=>{
     const [currButton, setCurrButton]= useState(0)
+    const swe = data.SWE_projs
+    const ML = data.ML_projs
+    const AV = data.AV_projs
 
+    console.log(swe)
     return(
         <div className="container" id="projects" style={{height:"100vh"}}>
-            <div className={styles.body}>
-                <h1>Projects</h1>
-                <div className="button-grid">
+            <div className={styles.projects}>
+                <h1>PROJECTS</h1>
+                <div className={styles.button_grid}>
                     <button onClick={()=>setCurrButton(0)}>
-                        Software Dev
+                        Software Dev & Product
                     </button>
                     <button onClick={()=>setCurrButton(1)}>
                         ML Research
@@ -20,10 +26,55 @@ const Projects =()=>{
                     <button onClick={()=>setCurrButton(2)}>
                         Autonomous Vehicles
                     </button>
+                    {/* <Image src={require('../alp.png')} /> */}
 
                 </div>
-                <div className = "projects-container">
-                    
+                <div className = {styles.projects_container}>
+                {currButton == 0 && 
+                    Object.entries(swe).map(([key, project]) => (
+                        <div className = {styles.project_item}><ProjectCard
+                          key={key}
+                          title={project.title}
+                          date={project.date}
+                          imgLink={project.imgLink}
+                          role={project.role}
+                          description={project.description}
+                          tools={project.tools}
+                          githubLink={project.githubLink}
+                          devpostLink={project.devpostLink}
+                          otherLink={project.otherLink}
+                        /></div>
+                      ))}
+                    {currButton == 1 && 
+                    Object.entries(ML).map(([key, project]) => (
+                        <div className = {styles.project_item}><ProjectCard
+                          key={key}
+                          title={project.title}
+                          date={project.date}
+                          imgLink={project.imgLink}
+                          role={project.role}
+                          description={project.description}
+                          tools={project.tools}
+                          githubLink={project.githubLink}
+                          devpostLink={project.devpostLink}
+                          otherLink={project.otherLink}
+                        /></div>
+                      ))}
+                    {currButton == 2 && 
+                    Object.entries(AV).map(([key, project]) => (
+                        <div className = {styles.project_item}><ProjectCard
+                          key={key}
+                          title={project.title}
+                          date={project.date}
+                          imgLink={project.imgLink}
+                          role={project.role}
+                          description={project.description}
+                          tools={project.tools}
+                          githubLink={project.githubLink}
+                          devpostLink={project.devpostLink}
+                          otherLink={project.otherLink}
+                        /></div>
+                      ))}
                 </div>
 
                 
