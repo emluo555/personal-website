512bdd3f3667a471c6a983af4c93bb5cfe64375e
emluo555 <emilyluo555@gmail.com>
2024-09-30 11:33:44 -0400

    projects section

--- app/Components/ProjectCard.jsx
+++ app/Components/ProjectCard.jsx
@@ -3,29 +3,35 @@ import React, {useState} from 'react';
 import styles from './Projects.module.css';
 import Image from "next/image";
 
-const ProjectCard =({title, imgLink, role, description, tools, githubLink, devpostLink})=>{
+const ProjectCard =({title, imgLink, date, role, description, tools, githubLink, devpostLink, otherLink})=>{
     console.log((imgLink))
     return(
-        <div className="container">
+        <div className={styles.pjc_container}>
             <img src={imgLink} width={"90%"}  ></img>
-            <h1 style={{fontSize:'100%'}}>{title}</h1>
-            <p>{description}</p>
+            <h1 style={{fontSize:'140%', marginTop:'3%'}}>{title}</h1>
+            {role != "" ? <span style={{display:'inline'}}><p className={styles.role}>{role}, </p><p className={styles.date}>{date}</p></span>
+            :  <span style={{display:'inline'}}><p className={styles.date}>{date}</p></span>}
+            
+            <div className={styles.desc_container}>
+                <p className={styles.desc_text}>{description}</p>
+            </div>
+            
             {tools != "" && 
-            <p>
-               {tools} 
+            <p className={styles.tools}>
+               Tools: {tools} 
             </p>}
+            <div className={styles.link_container}>
             {githubLink !="" &&
-                <span>
-                    <p></p>
-                    <a></a>
-                </span>
+                <a href={githubLink} target="_blank">Github</a>
             }
             {devpostLink !="" &&
-                <span>
-                    <p></p>
-                    <a></a>
-                </span>
+                <a href = {devpostLink} target="_blank">Devpost</a>
             }
+            {otherLink !="" &&
+                <a href = {otherLink} target="_blank">Website</a>
+            }
+            </div>
+            
         </div>
     )
     }
--- app/Components/Projects.jsx
+++ app/Components/Projects.jsx
@@ -1,8 +1,12 @@
 "use client"
-import React, {useState} from 'react';
+import React, {useState,useRef,useEffect} from 'react';
 import styles from './Projects.module.css';
 import data from '../data/projects.json';
 import ProjectCard from './ProjectCard';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 
 const Projects =()=>{
@@ -11,12 +15,34 @@ const Projects =()=>{
     const ML = data.ML_projs
     const AV = data.AV_projs
 
-    console.log(swe)
+    const projectsRef = useRef(null);
+
+  useEffect(() => {
+    const elements = projectsRef.current.querySelectorAll('.animate');
+
+    gsap.fromTo(
+      elements,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.out',
+        stagger: 0.3,
+        scrollTrigger: {
+          trigger: projectsRef.current,
+          start: "top 80%", // Start animation when the top of Projects is at 10% of viewport height
+          toggleActions: "play none none none", // Actions for entering and leaving the trigger area
+        },
+      }
+    );
+  }, []);
+
     return(
-        <div className="container" id="projects" style={{height:"100vh"}}>
-            <div className={styles.projects}>
-                <h1>PROJECTS</h1>
-                <div className={styles.button_grid}>
+        <div ref={projectsRef} className="container" id="projects" style={{height:"100vh"}}>
+    <div className={`animate ${styles.projects}`}>
+        <h1>PROJECTS</h1>
+                <div className={`animate ${styles.button_grid}`}>
                     <button onClick={()=>setCurrButton(0)}>
                         Software Dev & Product
                     </button>
@@ -29,7 +55,7 @@ const Projects =()=>{
                     {/* <Image src={require('../alp.png')} /> */}
 
                 </div>
-                <div className = {styles.projects_container}>
+                <div className = {`animate ${styles.projects_container}`}>
                 {currButton == 0 && 
                     Object.entries(swe).map(([key, project]) => (
                         <div className = {styles.project_item}><ProjectCard
