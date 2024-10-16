d3e5fe7572bbdb27b40f9f9af10612dfc8835643
emluo555 <emilyluo555@gmail.com>
2024-10-16 12:43:41 -0400

    animation fix new

--- app/page.js
+++ app/page.js
@@ -1,20 +1,36 @@
 
+'use client'
 import Image from "next/image";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Mainpage from './Components/Mainpage';
 import Projects from './Components/Projects';
 import Resume from './Components/Resume';
 import Navbar from "./Components/Navbar"
+import {useEffect, useRef} from 'react'
 
 // Register the ScrollTrigger plugin with GSAP
 //gsap.registerPlugin(ScrollTrigger);
+import gsap from 'gsap';
 
 
 export default function Home() {
+  const containerRef = useRef(null);
 
+  useEffect(() => {
+
+  let ctx = gsap.context(() => {
+    let t1 = gsap.timeline()
+    t1.from(["#to-animate"], {
+      opacity: 0,
+      duration:1,
+    })},containerRef);  // Attach context to the ref
+
+    return () => ctx.revert();  // Clean up GSAP context on unmount
+  }, []);
 
   return (
-    <div>
+    <div ref={containerRef}>
+      <div id="to-animate">
       <Navbar/>
       <Mainpage/>
 
@@ -22,6 +38,8 @@ export default function Home() {
       {/* <Resume/> */}
       
     </div>
+    </div>
+    
   );
 }
 
