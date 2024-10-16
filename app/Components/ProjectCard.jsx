f69d74c4e7cf036de01ecdeda3a3235ec10daa83
emluo555 <emilyluo555@gmail.com>
2024-10-16 13:17:24 -0400

    image updates

--- app/Components/ProjectCard.jsx
+++ app/Components/ProjectCard.jsx
@@ -2,13 +2,34 @@
 import React, {useState} from 'react';
 import styles from './Projects.module.css';
 import alp from "../../public/alp.png"
+import wakey from "../../public/wakey.png"
+import bundle from "../../public/bundle.png"
+import tryon from "../../public/tryon.png"
+import stereo from "../../public/stereo.png"
+import solar from "../../public/solar.png"
+import orf from "../../public/orf.png"
+import irom from "../../public/irom.png"
+import pcis from "../../public/pcis.png"
+
 
 
 const ProjectCard =({title, imgLink, date, role, description, tools, githubLink, devpostLink, otherLink})=>{
-    console.log(alp)
+    const imgs = {
+        "alp": alp,
+        "wakey": wakey,
+        "bundle": bundle,
+        "tryon": tryon,
+        "stereo": stereo,
+        "solar": solar,
+        "orf": orf,
+        "irom": irom,
+        "pcis": pcis
+    }
+    let source = imgs[imgLink].src
+
     return(
         <div className={styles.pjc_container}>
-            <img src={alp.src} width={"90%"}  ></img>
+            <img src={source} width={"90%"}  ></img>
             <h1 style={{fontSize:'140%', marginTop:'3%'}}>{title}</h1>
             {role != "" ? <span style={{display:'inline'}}><p className={styles.role}>{role}, </p><p className={styles.date}>{date}</p></span>
             :  <span style={{display:'inline'}}><p className={styles.date}>{date}</p></span>}
