bda30da89b7991109f3bddf854cb8a551b59598e
emluo555 <emilyluo555@gmail.com>
2025-11-11 22:44:23 -0500

    animation fixes

--- app/Components/Mainpage.jsx
+++ app/Components/Mainpage.jsx
@@ -7,43 +7,37 @@ import Spline from '@splinetool/react-spline/next';
 import dynamic from 'next/dynamic';
 //const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false });
 
-
-
-
 const Mainpage =()=>{
     const comp = useRef(null)
-    const words = ["a Software Dev & PM", "a Self-Driving Car Enthusiast", "an ML & AV Researcher", "a Hackathon Fan", "a Distance Runner & Dancer"]
+    const words = ["a Software Developer", "a Machine Learning & Robotics Researcher", "a Self-Driving Car Enthusiast", "a Hackathon Fan", "a Distance Runner & Dancer"]
     gsap.registerPlugin(TextPlugin);
 
 
     useLayoutEffect(() => {
-      
         let ctx = gsap.context(() => {
-          const t1 = gsap.timeline()
-          t1.from(["#title-1"], {
-              opacity: 0,
-              y: "-=300",
-              duration:1,
-            },0.2).from(["#title-2"], {
-                opacity: 0,
-                y: "+=300",
-                duration:1,
-              },0.2).from(["#splineview"], {
-                opacity: 0,
-                duration:2,
-              },0.2).
-              from(["#title-3"],{
-                opacity: 0,
-                y: "+=100",
-                duration:1,},1.5
-              ).
-              from(["#icons"],{
-                opacity: 0,
-                y: "+=100",
-                duration:1,},1.8
-              )
-
-        }, comp)
+        const t1 = gsap.timeline()
+        t1.fromTo("#title-1", 
+            { opacity: 0, y: -300 }, 
+            { opacity: 1, y: 0, duration: 1 },  
+            0.2
+        ).fromTo("#title-2",
+            { opacity: 0, y: 300 },
+            { opacity: 1, y: 0, duration: 1 },
+            0.2
+        ).fromTo("#splineview",
+            { opacity: 0 },
+            { opacity: 1, duration: 2 },
+            0.2
+        ).fromTo("#title-3",
+            { opacity: 0, y: 100 },
+            { opacity: 1, y: 0, duration: 1 },
+            1.5
+        ).fromTo("#icons",
+            { opacity: 0, y: 100 },
+            { opacity: 1, y: 0, duration: 1 },
+            1.8
+        )
+    }, comp)
         let ctx2 = gsap.context(()=> {
             gsap.to("#cursor",{
                 opacity: 0,
@@ -70,10 +64,10 @@ const Mainpage =()=>{
     return(
         <div className={styles.container}id="mainpage" ref={comp}>
             <div className={styles.text}>
-                <h2 id="title-1">Hi! My name is</h2>
-                <h1 id="title-2">Emily Luo</h1>
-                <span id="title-3" style={{ display: "flex", width:"150%" }}><h3 >I am</h3><h3 id="animated-text" style={{marginLeft:"6px"}}></h3><h3 id="cursor">_</h3></span>
-                <div id="icons" className=''>
+                <h2 id="title-1" style={{ opacity: 0, transform: 'translateY(-300px)' }}>Hi! My name is</h2>
+                <h1 id="title-2" style={{ opacity: 0, transform: 'translateY(-300px)' }}>Emily Luo</h1>
+                <span id="title-3" style={{ display: "flex", width:"150%", opacity: 0, transform: 'translateY(100px)' }}><h3 >I am</h3><h3 id="animated-text" style={{marginLeft:"6px"}}></h3><h3 id="cursor">_</h3></span>
+                <div id="icons" style={{ opacity: 0, transform: 'translateY(100px)' }} className='' >
                     <a href="https://github.com/emluo555" target="_blank">
                     <img className={styles.icon_item} src='/github.png' alt="github" />
                     </a>
